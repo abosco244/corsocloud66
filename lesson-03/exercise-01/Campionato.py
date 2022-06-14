6d561dead81bc507f1a2526c778c492c82dc77eb
Aldo <aldo.ravone@gmail.com>
2022-06-14 16:57:41 +0200

    Aggiunti metodo random, metodo empty_score e metodo range_distance in class Campionato

--- lesson-03/exercise-01/Campionato.py
+++ lesson-03/exercise-01/Campionato.py
@@ -1,3 +1,5 @@
+import random
+
 class Campionato:
          
     # Imposta il dizionario
@@ -6,7 +8,20 @@ class Campionato:
     
     def add_team(self, nome, punteggio):
         self.__s[nome] = punteggio
- 
+
+    def add_team_empty_score(self, nome):
+        self.__s[nome] = 0
+
+    def set_random_score(self):
+        for key in self.__s.keys():
+            self.__s[key] = random.randint(1, 50)
+            
+    def range_distance_team(self):
+        first_team = self.get_first_team()
+        last_team = self.get_last_team()
+        return f"La distanza massima tra {first_team[0]} e {last_team[0]} è di {first_team[1] - last_team[1]} punti"
+                              
+
     def get_team(self, nome):
         self.__s.get(nome,-10)
  
@@ -19,7 +34,7 @@ class Campionato:
             for key in self.__s:
                 print(self.__s)
                 if self.__s[key]==m:
-                    return key, m
+                    return [key, m]
         else:
             return "",0
  
@@ -28,7 +43,7 @@ class Campionato:
             m=min(self.__s.values())
             for key in self.__s:
                 if self.__s[key]==m:
-                    return key, m
+                    return [key, m]
         else:
             return "",0
  
@@ -45,97 +60,8 @@ class Campionato:
 
 
 
-def main():
-    c = Campionato()
-    n = int(input("Da quante squadre e' composto il campionato?"))
-    for i in range(0, n):
-
-        nome_squadra = input("Inserire il nome della squadra: ")
-
-        punteggio = int(input("Inserire il punteggio ottenuto dalla squadra "+nome_squadra+": "))
-
-        c.add_team(nome_squadra, punteggio)
-
-    print("Il campionato e' formato da "+str(c.get_number_of_team())+" squadre")
-
-    if c.get_number_of_team()>1:
-
-        squadra, p = c.get_first_team()
-
-        print("La squadra che ha vinto il campionato e' "+squadra+" con un punteggio di "+str(p)+" punti")
-
-    if c.get_number_of_team()>0:
-
-        print("Le ultime 3 squadre del campionato sono:")
-
-        #s servirá per ripristinare tutte le squadre cancellate 
-
-        s = dict()
-
-        for i in range(0,3):
-
-            squadra, p = c.get_last_team()
-            print("get_last_team: " + str(c.get_last_team()))
-            print("squadra: " + str(squadra))
-            print("p: " + str(p))
-
-            s[squadra]=p
-
-            c.delete_team(squadra)
-
-            print(squadra+" con un punteggio di "+str(p)+" punti")
-
- 
-
-        #ripristina lo stato del campionato
-
-        for k in s:
-
-            c.add_team(k, s[k])
-
- 
-
-    #verifichiamo che il numero delle squadre e lo stato del campionato sia lo stesso di quello iniziale
-
-    print("Il campionato e' formato da "+str(c.get_number_of_team())+" squadre")
-
-    print(c)
-
- 
-
-main()
 
 
 
 
-# def run():
-#     campionato = Campionato()
-#     inp = int(input("Inserisci il numero di squadre di cui sarà composto il nostro campionato: "))
-    
-#     for i in range(inp):
-#         team = str(input("Inserisci il nome della squadra: "))
-#         score = int(input(f"Inserisci il punteggio della squadra {team}: "))
-#         campionato.add_team(team, score)
-        
-#     squadra, p = campionato.get_first_team()
-#     print("La squadra che ha vinto il campionato e' "+squadra+" con un punteggio di " + str(p) + " punti")
-    
-#     print(campionato.get_first_team())
-#     print(campionato.get_last_team())
-    
-#     suqadra, punteggio = campionato.get_first_team()
-    
-#     for i in range(0,3):
-        
-#         squadra, p = c.get_last_team()
 
-#         s[squadra]=p
-
-#         c.delete_team(squadra)
-
-#         print(squadra+" con un punteggio di "+str(p)+" punti")
-        
-        
-    
-        
-# run()

6a18528a7c77d48c99fae46e6f5f879edf421d8f
Dev1 - Tesan <tesiu1234@gmail.com>
2022-06-15 13:06:33 +0200

    Aggiunto classi con mysql

--- lesson-03/exercise-01/Connessione_query.py
+++ lesson-03/exercise-01/Connessione_query.py
@@ -1,3 +1,5 @@
+# Tira fuori tutti gli attori che hanno partecipato al film african egg (nome, cognome)
+
 # Importazione del modulo MySQL
 import mysql.connector
 
@@ -14,16 +16,15 @@ try:
     # Generazione del cursore
     cursore = connessione.cursor()
     # Comando SQL per la visualizzazione dei database
-    cursore.execute("select * from Actor")
+    cursore.execute("select a.first_name, a.last_name from film f, actor a, film_actor fa where (f.film_id = fa.film_id and a.actor_id = fa.actor_id and f.title = 'AFRICAN EGG')")
     # get all records
     records = cursore.fetchall()
     print("Total number of rows in table: ", cursore.rowcount)
 
     print("\nPrinting each row")
     for row in records:
-            print("Id = ", row[0], )
-            print("Name = ", row[1])
-            print("Lastname = ", row[2])
+            print("Name = ", row[0])
+            print("Lastname = ", row[1])
             
 except mysql.connector.Error as e:
     print("Error reading data from MySQL table", e)
